1a3e79c8a47322020604fb09d6bd3f5e8553a06c
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2016-04-22 08:49:14 +0900

    to Serializable

--- junit_ws02/src/main/java/com/example/ui/CalculationPage.java
+++ junit_ws02/src/main/java/com/example/ui/CalculationPage.java
@@ -1,7 +1,9 @@
 package com.example.ui;
 
+import com.example.data.SquareLogBean;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 
 public class CalculationPage extends WebPage {
@@ -9,5 +11,11 @@ public class CalculationPage extends WebPage {
 	public CalculationPage(IModel<Double> valueModel, IModel<Double> squareModel) {
 		queue(new Label("value", valueModel));
 		queue(new Label("square", squareModel));
+	}
+
+	public CalculationPage(IModel<SquareLogBean> squareLogBeanModel) {
+		setDefaultModel(CompoundPropertyModel.of(squareLogBeanModel));
+		queue(new Label("value"));
+		queue(new Label("value"));
 	}
 }
